88d00514e9293535fcca56c4b503cbd61ca46f72
EiadToma <eiadtoma@gmail.com>
2022-08-30 19:15:00 +0300

    taskamanger

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import Tasks from './components/Tasks'
+import {useState,useEffect} from 'react'
+import Header from './components/Header'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask,setShowAddTask]= useState(true)
+  const [tasks, setTasks]=useState([])
+useEffect(()=>{
+const getTasks = async()=>{
+  const tasksFromServer = await fetchTasks()
+  setTasks(tasksFromServer)
+}
+  getTasks()
+},[])
+
+const fetchTasks = async()=>{
+  const res= await fetch(`http://localhost:4000/Tasks`)
+  const data = await res.json()
+  return data
+}
+const fetchTask = async(id)=>{
+  const res= await fetch(`http://localhost:4000/Tasks/${id}`)
+  const data = await res.json()
+  return data
+}
+
+
+const addTask =async (task)=>{
+const res = await fetch('http://localhost:4000/Tasks',
+{
+  method:'POST',
+  headers:{
+    'Content-type':'application/json'
+  },body:JSON.stringify(task),
+})
+const data =  await res.json()
+setTasks([...tasks,data])
+
+  // const id= Math.floor(Math.random()*10000)+1
+// const newtask = {id,...task}
+// setTasks([...tasks,newtask])
+
+}
+
+
+
+const deleteTask =async(id)=> {
+  await fetch(`http://localhost:4000/Tasks/${id}`,
+  {method:'DELETE'
+})
+setTasks(tasks.filter((task) =>task.id !==id
+))
+}
+
+const toggleReminder =async (id) =>{
+const taskToToggle= await fetchTask(id)
+const updTask = {...taskToToggle,reminder:!taskToToggle.reminder}
+const res=await fetch(`http://localhost:4000/Tasks/${id}`,{
+  method:'PUT',
+  headers:{
+    'content-type':'application/json'
+  },
+  body:JSON.stringify(updTask)
+})
+
+const data =await res.json()
+
+setTasks(tasks.map((task)=>task.id===id
+  ? {...task,reminder:data.reminder}:task))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Header onAdd={()=>setShowAddTask(!showAddTask)}  showAdd={showAddTask }/>
+
+     {showAddTask && <AddTask onAdd={addTask} />}
+     {tasks.length> 0 ? <Tasks tasks={tasks} 
+     onDelete={deleteTask}
+     onToggle={toggleReminder}
+      /> : 'no tasks'}
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({h1,onAdd,showAdd}) => {
+    
+  return (
+    <header className='header'>
+      <h1>{h1} </h1>
+      <Button text={showAdd?'close':'add'} color='black' onClick={onAdd}  />
+    </header>
+  )
+}
+Header.defaultProps ={
+    h1:'nibba here i come',
+}
+Header.propTypes={
+    h1:PropTypes.string.isRequired
+}
+
+export default Header
